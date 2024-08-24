1e16a645e205a5337d1717d56751a29a24132e19
Michal Kukla <michal.kukla91@gmail.com>
2024-08-24 16:22:18 +0200

    install "sass" and "modern-normalize" to normalize default styles for browsers

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: ["@nuxt/image"],
+  css: ["~/node_modules/modern-normalize/modern-normalize.css"],
+});
