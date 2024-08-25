85e0cf23d561623a94ac73c0c35fd850bf4ce1c2
Michal Kukla <michal.kukla91@gmail.com>
2024-08-25 16:35:53 +0200

    install google-fonts and add Montserrat font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,24 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxt/image"],
+  modules: ["@nuxt/image", "@nuxtjs/google-fonts"],
   css: ["~/node_modules/modern-normalize/modern-normalize.css"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/scss/_variables.scss" as *;',
+        },
+      },
+    },
+  },
+
+  googleFonts: {
+    families: {
+      Montserrat: {
+        wght: "600",
+      },
+    },
+    display: "swap",
+  },
 });
