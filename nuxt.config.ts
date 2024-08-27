80dedb1a517a3067b841f5c5a0c1b65f038181b1
Michal Kukla <michal.kukla91@gmail.com>
2024-08-27 16:07:51 +0200

    add scss variables and mixins

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,8 @@ export default defineNuxtConfig({
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@use "~/assets/scss/_variables.scss" as *;',
+          additionalData: `@use "~/assets/scss/_variables.scss" as *;
+            @use "~/assets/scss/_mixins.scss" as *;`,
         },
       },
     },
