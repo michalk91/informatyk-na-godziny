8e7e097309bc6595527c7e84b5e62fdff3cd2acf
Michal Kukla <michal.kukla91@gmail.com>
2024-08-29 23:48:22 +0200

    create <Modal />

---
+++ components/Modal.vue
@@ -0,0 +1,30 @@
+<template>
+  <teleport to="body">
+    <div v-if="isOpen" @click="$emit('closeModal')" class="modal-overlay">
+      <div class="modal-content" @click.stop>
+        <slot />
+      </div>
+    </div>
+  </teleport>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  isOpen: boolean;
+}>();
+</script>
+
+<style>
+.modal-overlay {
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 1000;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
