e379afb0a4a3c0196e3fe1cf638c1039cc4a3aba
Michal Kukla <michal.kukla91@gmail.com>
2024-08-31 12:55:20 +0200

    separate styles for hamburger navigation and non-hamburger navigation

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -13,11 +13,10 @@
         <span></span>
       </div>
     </div>
-    <div :class="{ background: !withoutHamburgerMenu }"></div>
+    <div v-if="!withoutHamburgerMenu" class="background"></div>
     <ul
       :class="{
-        showNav: menuOpen,
-        'without-hamburger': withoutHamburgerMenu,
+        showNav: menuOpen && !withoutHamburgerMenu,
       }"
     >
       <li v-for="item of items">
@@ -45,98 +44,122 @@ function handleClick() {
 
 <style scoped lang="scss">
 nav {
-  .showNav {
-    .without-hamburger {
-      padding: 0;
-      margin: 0;
-    }
+  &.without-hamburger {
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+      list-style: none;
+      column-gap: 40px;
+      row-gap: 20px;
+      transition: 0.4s;
+      position: relative;
+      z-index: 7;
+      padding-inline-start: 0px;
+      column-gap: 40px;
 
-    &:not(.without-hamburger) {
-      transform: translate(-50%, 0);
-    }
-  }
+      li {
+        display: flex;
+        align-items: center;
+        font-size: 14px;
+        line-height: 17.07px;
 
-  .background {
-    @media screen and (max-width: 1150px) {
-      position: absolute;
-      z-index: 5;
-      top: 0;
-      left: 0;
-      bottom: 0;
-      right: 0;
-      background: $background-color;
-    }
-  }
-
-  .menuToggle {
-    display: none;
-    position: relative;
-    overflow: hidden;
-
-    @media screen and (max-width: 1150px) {
-      display: block;
-      -webkit-user-select: none;
-      user-select: none;
-      padding: 32px 102px 32px 32px;
-
-      @media screen and (max-width: 510px) {
-        padding: 40px 40px 40px 40px;
+        &:hover {
+          cursor: pointer;
+          color: #ffa500;
+        }
       }
+    }
+  }
+  &:not(.without-hamburger) {
+    .showNav {
+      transform: translate(-50%, 0);
 
-      input {
-        display: block;
-        width: 100%;
-        height: 100%;
+      .without-hamburger {
+        padding: 0;
+        margin: 0;
+      }
+    }
+
+    .background {
+      @media screen and (max-width: 1150px) {
         position: absolute;
+        z-index: 5;
         top: 0;
         left: 0;
-        cursor: pointer;
-        opacity: 0;
-        z-index: 7;
-        -webkit-touch-callout: none;
-
-        &:checked ~ span:nth-child(2) {
-          opacity: 1;
-          transform: translateY(11px) rotate(45deg);
-        }
-        &:checked ~ span:nth-child(3) {
-          opacity: 0;
-          transform: rotate(0deg) translateX(5px);
-        }
-
-        &:checked ~ span:nth-child(4) {
-          transform: translateY(-11px) rotate(-45deg);
-        }
+        bottom: 0;
+        right: 0;
+        background: $background-color;
       }
-      span {
+    }
+
+    .menuToggle {
+      display: none;
+      position: relative;
+      overflow: hidden;
+
+      @media screen and (max-width: 1150px) {
         display: block;
-        width: 40px;
-        height: 5px;
-        margin-bottom: 6px;
-        position: relative;
-        background: #cdcdcd;
-        border-radius: 3px;
-        z-index: 6;
-        transition: transform 0.5s cubic-bezier(0.77, 0.2, 0.05, 1),
-          background 0.5s cubic-bezier(0.77, 0.2, 0.05, 1), opacity 0.55s ease;
+        -webkit-user-select: none;
+        user-select: none;
+        padding: 32px 102px 32px 32px;
+
+        @media screen and (max-width: 510px) {
+          padding: 40px 40px 40px 40px;
+        }
+
+        input {
+          display: block;
+          width: 100%;
+          height: 100%;
+          position: absolute;
+          top: 0;
+          left: 0;
+          cursor: pointer;
+          opacity: 0;
+          z-index: 7;
+          -webkit-touch-callout: none;
+
+          &:checked ~ span:nth-child(2) {
+            opacity: 1;
+            transform: translateY(11px) rotate(45deg);
+          }
+          &:checked ~ span:nth-child(3) {
+            opacity: 0;
+            transform: rotate(0deg) translateX(5px);
+          }
+
+          &:checked ~ span:nth-child(4) {
+            transform: translateY(-11px) rotate(-45deg);
+          }
+        }
+        span {
+          display: block;
+          width: 40px;
+          height: 5px;
+          margin-bottom: 6px;
+          position: relative;
+          background: #cdcdcd;
+          border-radius: 3px;
+          z-index: 6;
+          transition: transform 0.5s cubic-bezier(0.77, 0.2, 0.05, 1),
+            background 0.5s cubic-bezier(0.77, 0.2, 0.05, 1), opacity 0.55s ease;
+        }
       }
     }
-  }
-  ul {
-    display: flex;
-    flex-wrap: wrap;
-    list-style: none;
-    gap: 40px;
-    transition: 0.4s;
-    position: relative;
-    z-index: 7;
-    padding-inline-start: 0px;
-
-    &:not(.without-hamburger) {
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+      list-style: none;
+      column-gap: 40px;
+      row-gap: 20px;
+      transition: 0.4s;
+      position: relative;
+      z-index: 7;
+      padding-inline-start: 0px;
       padding: 32px 102px 32px 0;
-    }
+      column-gap: 40px;
+      row-gap: 60px;
 
-    &:not(.without-hamburger) {
       @media screen and (max-width: 1150px) {
         flex-direction: column;
         position: absolute;
@@ -150,14 +173,14 @@ nav {
         background-color: #5e607a;
         z-index: 2;
       }
-    }
 
-    li {
-      display: flex;
-      align-items: center;
-      &:hover {
-        cursor: pointer;
-        color: #ffa500;
+      li {
+        display: flex;
+        align-items: center;
+        &:hover {
+          cursor: pointer;
+          color: #ffa500;
+        }
       }
     }
   }
