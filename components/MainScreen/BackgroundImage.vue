bbf72d90534b75d2c5cbd6d5aaea5216fea0f456
Michal Kukla <michal.kukla91@gmail.com>
2024-09-02 19:57:10 +0200

    start animation only when image is loaded

--- components/MainScreen/BackgroundImage.vue
+++ components/MainScreen/BackgroundImage.vue
@@ -4,24 +4,36 @@
     preload
     alt="Background overlay"
     class="image"
+    :class="{ 'with-animation': imgLoaded }"
     height="917"
     width="1137"
     quality="80"
+    @load="handleImgLoaded"
   />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const imgLoaded = shallowRef(false);
+
+function handleImgLoaded() {
+  imgLoaded.value = true;
+}
+</script>
 
 <style scoped lang="scss">
 .image::v-deep(img) {
   height: auto;
   width: 1137px;
   max-width: 100%;
-  animation: fadeInRight 0.5s ease-in;
+  opacity: 0;
 
   @media screen and (max-width: 1500px) {
     display: none;
   }
+}
+
+.with-animation::v-deep(img) {
+  animation: fadeInRight 0.5s ease-in forwards;
 }
 
 @keyframes fadeInRight {
